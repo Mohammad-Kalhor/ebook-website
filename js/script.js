abef24338082c3f5c726950bbd67db5b8781873e
hazy <mohammad.mahdi.kalhor.98@gmail.com>
2023-09-09 11:28:36 -0700

    final

---
+++ js/script.js
@@ -0,0 +1,21 @@
+function userScroll() {
+  const navbar = document.querySelector(".navbar");
+
+  if (window.scrollY > 50) {
+    navbar.classList.add("bg-dark")
+    navbar.classList.add("navbar-sticky")
+  } 
+
+  window,
+    addEventListener("scroll", () => {
+      if (window.scrollY > 50) {
+        navbar.classList.add("bg-dark");
+        navbar.classList.add("navbar-sticky");
+      } else {
+        navbar.classList.remove("bg-dark");
+        navbar.classList.remove("navbar-sticky");
+      }
+    });
+}
+
+window.addEventListener("DOMContentLoaded", userScroll)
